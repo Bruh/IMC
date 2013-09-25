dba0696947bcf544b3d7815bba7e6fb2e71b6d2e
Bruno Silva <blackops_bruninho1994@hotmail.com>
2013-09-25 09:27:23 -0300

    Falta fazer ajustes

---
+++ IMC/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCal = new System.Windows.Forms.Button();
+            this.buttonLim = new System.Windows.Forms.Button();
+            this.buttonSair = new System.Windows.Forms.Button();
+            this.textBoxPeso = new System.Windows.Forms.TextBox();
+            this.textBoxAltura = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonCal
+            // 
+            this.buttonCal.Location = new System.Drawing.Point(12, 215);
+            this.buttonCal.Name = "buttonCal";
+            this.buttonCal.Size = new System.Drawing.Size(86, 35);
+            this.buttonCal.TabIndex = 0;
+            this.buttonCal.Text = "&Calcular";
+            this.buttonCal.UseVisualStyleBackColor = true;
+            this.buttonCal.Click += new System.EventHandler(this.buttonCal_Click);
+            // 
+            // buttonLim
+            // 
+            this.buttonLim.Location = new System.Drawing.Point(104, 215);
+            this.buttonLim.Name = "buttonLim";
+            this.buttonLim.Size = new System.Drawing.Size(75, 35);
+            this.buttonLim.TabIndex = 1;
+            this.buttonLim.Text = "&Limpar";
+            this.buttonLim.UseVisualStyleBackColor = true;
+            this.buttonLim.Click += new System.EventHandler(this.buttonLim_Click);
+            // 
+            // buttonSair
+            // 
+            this.buttonSair.Location = new System.Drawing.Point(183, 215);
+            this.buttonSair.Name = "buttonSair";
+            this.buttonSair.Size = new System.Drawing.Size(75, 35);
+            this.buttonSair.TabIndex = 2;
+            this.buttonSair.Text = "&Sair";
+            this.buttonSair.UseVisualStyleBackColor = true;
+            this.buttonSair.Click += new System.EventHandler(this.buttonSair_Click);
+            // 
+            // textBoxPeso
+            // 
+            this.textBoxPeso.Location = new System.Drawing.Point(12, 53);
+            this.textBoxPeso.Name = "textBoxPeso";
+            this.textBoxPeso.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPeso.TabIndex = 3;
+            // 
+            // textBoxAltura
+            // 
+            this.textBoxAltura.Location = new System.Drawing.Point(12, 137);
+            this.textBoxAltura.Name = "textBoxAltura";
+            this.textBoxAltura.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAltura.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Informe seu peso:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Informe sua altura:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAltura);
+            this.Controls.Add(this.textBoxPeso);
+            this.Controls.Add(this.buttonSair);
+            this.Controls.Add(this.buttonLim);
+            this.Controls.Add(this.buttonCal);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IMC|PJS|1241001";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCal;
+        private System.Windows.Forms.Button buttonLim;
+        private System.Windows.Forms.Button buttonSair;
+        private System.Windows.Forms.TextBox textBoxPeso;
+        private System.Windows.Forms.TextBox textBoxAltura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ IMC/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace IMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonLim_Click(object sender, EventArgs e)
+        {
+            textBoxPeso.Clear();
+            textBoxAltura.Clear();
+        }
+
+        private void buttonCal_Click(object sender, EventArgs e)
+        {
+            double IMC, PESO, ALTURA;
+            ALTURA = Convert.ToDouble(Convert.ToString(textBoxAltura.Text));
+            PESO = Convert.ToDouble(Convert.ToString(textBoxPeso.Text));
+            IMC = PESO / (ALTURA * ALTURA);
+
+            if (IMC <= 18.5)
+            {
+                MessageBox.Show( "Com IMC "+ IMC +".\n Nesse ponto, o corpo magro deixa de ser saudável e o organismo fica mais vulnerável a infecções.Se o baixo peso persistir,  mesmo com a alimentação normal, procure ajuda médica; a dififuldade para engordar pode ser sintoma de alguma doença insidiosa, como o diabetes.", "Seu resultado foi BAIXO PESO");    
+            }
+
+            if (IMC >= 18.5 && IMC <= 24.9)
+            {
+                MessageBox.Show("Com IMC " + IMC + ".\n Seu peso está adequado à altura. É importante manter a educação alimentar e a atividade física. Fique atento seu valor esteja perto dos limites para os estágios de BAIXO PESO ou SOBREPESO.", "Seu resultado foi PESO NORMAL ");
+            }
+
+            if (IMC >= 24.9 && IMC <= 29.9)
+            {
+                MessageBox.Show("Com IMC " + IMC + ".\n Está faixa indica que você está com predisposição à obesidade; dependendo do seu histórico familiar e pessoal, pode haver um quadro de pré-diabetes e hipertensão. Procure orientação médica para evitar o próximo estágio, OBESIDADE 1. Quanto mais quilos extras, maior a dificuldade para emagrecer.", "Seu resultado foi SOBREPESO");
+            }
+
+            if (IMC >= 29.9 && IMC <= 34.9)
+            {
+                MessageBox.Show("Com IMC " + IMC + ".\n O risco de desenvolver diabetes,para quem está nesta faixa de peso, é de 20%, e o de hipertensão ultrapassa 25%,Há risco maior de outras doenças cardiovasculares, articulares, distúrbios, psiquiátricos, apneia do sono e até certos tipos de cancêr.", "Seu resultado foi OBESIDADE GRAU 1 ");
+            }
+
+            if (IMC >= 34.9 && IMC <= 39.9)
+            {
+                MessageBox.Show("Com IMC " + IMC + ".\n O risco de desenvolver diabetes chega a 40% e a chance de surgirem doenças associadas à obsidade, como reumatismos, cancêr, apneia do sono, hipertensão e outros problemas cardiovasculares pode chegar a 50%.Procure orientação médica.", "Seu resultado foi OBSIDADE GRAU 2");
+            }
+
+            if (IMC >= 39.9 && IMC <= 40)
+            {
+                MessageBox.Show("Com IMC " + IMC + ".\n O risco de desenvolver doenças associados ao excesso de peso, como diabetes, reumatismo, cancêr, apneia do sono, hipertensão e outros problemas cardiovasculares chega a até 90%. Neste estágio, a cirurgia de redução do estômago pode ser indicada. Procure orinetação médica imediatamente.", "Seu resultado foi OBSIDADE GRAU 3");
+            }
+
+            
+        }
+    }
+}
